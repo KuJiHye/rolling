5bd9f5e11814e5416fdf738b32f301a97724a097
eugene <eugenekmpark@gmail.com>
2026-03-05 17:00:42 +0900

    feat: 버튼 컴포넌트 onClick 기능 추가

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,20 +1,24 @@
 import styled from "styled-components";
 
-function CreateMessageButton({ className }) {
-  return <button className={className}>생성하기</button>;
+function CreateMessageButton({ className, onClick, disabled }) {
+  return (
+    <button disabled={disabled} onClick={onClick} className={className}>
+      생성하기
+    </button>
+  );
 }
 
 const Button = styled(CreateMessageButton)`
   width: 720px;
   height: 56px;
   color: white;
-  background-color: #9935ff;
+  background-color: ${({ disabled }) => (disabled ? "grey" : "#9935ff")};
   border-radius: 12px;
   border: none;
   cursor: pointer;
 
-  &:hover,
-  &:active {
+  &:hover:not(:disabled),
+  &:active:not(:disabled) {
     background-color: #7615d6;
   }
 `;
