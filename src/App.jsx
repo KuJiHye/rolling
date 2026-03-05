9d6276d2debfb9fda2a92f02e6e1eca6627436d0
김윰 <kimyumin71@gmail.com>
2026-03-06 00:33:19 +0900

    feat: 메인 페이지 구현 및 라우팅 환경 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MainHeader from './components/MainHeader'; 
+import MainHeader from './components/MainHeader';
 import RollingPaperDetail from './pages/RollingPaperDetail';
+import MainPage from './pages/MainPage';
 
-function App() {
+const App = () => {
   return (
     <Router>
       <MainHeader /> 
       
       <Routes>
-        <Route path="/" element={<div>메인 페이지 내용</div>} />
-
-        <Route path="/post" element={<div>만들기 페이지</div>} />
+        <Route path="/" element={<MainPage />} />
+        <Route path="/post" element={<div>롤링 페이퍼 만들기 페이지</div>} />
         <Route path="/post/:id" element={<RollingPaperDetail />} />
-
-        <Route path="/list" element={<div>목록 페이지</div>} />        
+        <Route path="/list" element={<div>롤링 페이퍼 목록 페이지</div>} />        
       </Routes>
     </Router>
   );
-}
-
+};
 
 export default App;
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,13 @@
+//서비스의 메인 랜딩 페이지 컴포넌트
+import React from 'react';
+import ExploreButton from '../components/ExploreButton';
+
+const MainPage = () => {
+  return (
+    <div>
+      <ExploreButton /> 
+    </div>
+  );
+};
+
+export default MainPage;
