c61c3cb9d84cb492c6376b6169c8351eac1d2f34
chaemin <sulv0309@gmail.com>
2026-03-06 16:48:51 +0900

    feat: post 페이지와 main 페이지 연결

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainHeader from './components/MainHeader';
 import RollingPaperDetail from './pages/RollingPaperDetail';
 import MainPage from './pages/MainPage';
+import Post from './pages/Post';
 
 const App = () => {
   return (
@@ -11,7 +12,7 @@ const App = () => {
 
       <Routes>
         <Route path="/" element={<MainPage />} />
-        <Route path="/post" element={<div>롤링 페이퍼 만들기 페이지</div>} />
+        <Route path="/post" element={<Post />} />
         <Route path="/post/:id" element={<RollingPaperDetail />} />
         <Route path="/list" element={<div>롤링 페이퍼 목록 페이지</div>} />        
       </Routes>
