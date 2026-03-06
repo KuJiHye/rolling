ba0163d4875a1ec5f5d2f180f706f60d309ea799
chaemin <sulv0309@gmail.com>
2026-03-06 17:11:01 +0900

    feat: list 페이지 연결

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,17 @@ import MainHeader from './components/MainHeader';
 import RollingPaperDetail from './pages/RollingPaperDetail';
 import MainPage from './pages/MainPage';
 import Post from './pages/Post';
+import ListPage from './pages/ListPage';
 
 const App = () => {
   return (
     <Router>
       <MainHeader /> 
-
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/post" element={<Post />} />
         <Route path="/post/:id" element={<RollingPaperDetail />} />
-        <Route path="/list" element={<div>롤링 페이퍼 목록 페이지</div>} />        
+        <Route path="/list" element={<ListPage />} />        
       </Routes>
     </Router>
   );
