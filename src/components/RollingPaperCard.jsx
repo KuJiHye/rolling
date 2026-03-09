f2d86b98692e032b9350170d8f7e03598a335044
Yuseung Sung <usngsung0830@naver.com>
2026-03-09 16:58:40 +0900

    feat: 카드마다 배경화면 색상 추가

--- src/components/RollingPaperCard.jsx
+++ src/components/RollingPaperCard.jsx
@@ -3,7 +3,20 @@ import styled from "styled-components";
 
 const CardWrapper = styled.div`
   cursor: pointer;
+  width: 275px;
+  height: 260px;
+  border: 1px solid var(--black);
+  border-radius: 16px;
+
+  background-color: ${({ $colorName }) => colorMatching[$colorName] || '#ee3131'};
 `;
+
+const colorMatching = {
+  beige: '#FFE2AD',
+  purple: '#ECD9FF',
+  blue: '#B1E4FF',
+  green: '#D0F5C3',
+};
 
 function RollingPaperCard({ list }) {
   const navigate = useNavigate();
@@ -13,24 +26,24 @@ function RollingPaperCard({ list }) {
   };
 
   return (
-    <CardWrapper onClick={handleCardClick}>
+    <CardWrapper onClick={handleCardClick} $colorName={list.backgroundColor} >
       <h2>{list.name}</h2>
-      <span>{list.messageCount}</span>
-      <ul>
+      {/* <span>{list.messageCount}</span> */}
+      {/* <ul>
         {list.recentMessages.map((message) => (
           <li key={message.id}>
             <img src={message.profileImageURL}></img>
           </li>
         ))}
-      </ul>
-      <ul>
+      </ul> */}
+      {/* <ul>
         {list.topReactions.map((reaction) => (
           <li key={reaction.id}>
             {reaction.emoji}
             {reaction.count}
           </li>
         ))}
-      </ul>
+      </ul> */}
     </CardWrapper>
   );
 }
