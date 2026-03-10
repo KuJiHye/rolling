1152151b0adfd83bcc38f52eea61a6b232f9ccce
김윰 <kimyumin71@gmail.com>
2026-03-10 12:55:16 +0900

    feat: 카카오톡 공유 기능 구현 및 SDK 최신화

--- src/components/KakaoShareButton.jsx
+++ src/components/KakaoShareButton.jsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const KakaoShareButton = ({ onClose }) => {
+  useEffect(() => {
+    if (window.Kakao) {
+      const kakao = window.Kakao;
+      if (!kakao.isInitialized()) {
+        kakao.init('fc2a5198027a4e4488c14744921cf307'); 
+      }
+    }
+  }, []);
+
   const handleShare = () => {
+    if (window.Kakao) {
+      window.Kakao.Share.sendDefault({
+        objectType: 'feed',
+        content: {
+          title: '임시 제목', 
+          description: '임시 설명',
+          imageUrl: '임시', // 공유 시 보여줄 이미지 주소
+          link: {
+            mobileWebUrl: window.location.href, 
+            webUrl: window.location.href,
+          },
+        },
+        buttons: [
+          {
+            title: '메시지 쓰러 가기',
+            link: {
+              mobileWebUrl: window.location.href,
+              webUrl: window.location.href,
+            },
+          },
+        ],
+      });
+    }
+
     onClose(); 
   };
 
