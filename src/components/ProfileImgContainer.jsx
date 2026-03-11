4d375e348d9b68dd40ff5faf576e69977c75cf6d
eugene <eugenekmpark@gmail.com>
2026-03-11 18:11:10 +0900

    refactor: StyledLabel 컴포넌트화

--- src/components/ProfileImgContainer.jsx
+++ src/components/ProfileImgContainer.jsx
@@ -1,32 +1,19 @@
 import ProfileImg from "./ProfileImg";
 import ProfileImgOptions from "./ProfileImgOptions";
 import styled from "styled-components";
+import StyledLabel from "./StyledLabel";
 
 function ProfileImgContainer({ label, value, changeImg }) {
   return (
-    <StyledWrapper>
+    <div>
       <StyledLabel>{label}</StyledLabel>
       <StyledOptions>
         <ProfileImg value={value} />
         <ProfileImgOptions changeImg={changeImg} />
       </StyledOptions>
-    </StyledWrapper>
+    </div>
   );
 }
-
-const StyledWrapper = styled.div``;
-
-const StyledLabel = styled.label`
-  color: #181818;
-  font-family: Pretendard;
-  font-size: 24px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 36px; /* 175% */
-  letter-spacing: -1%;
-  display: block;
-  margin-bottom: 12px;
-`;
 
 const StyledOptions = styled.div`
   display: flex;
---
+++ src/components/StyledLabel.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const StyledLabel = styled.label`
+  color: #181818;
+  font-family: Pretendard;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 36px;
+  letter-spacing: -1%;
+  display: block;
+  margin-bottom: 12px;
+`;
+
+export default StyledLabel;
