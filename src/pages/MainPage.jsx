fa122bebe8bf662a0b09376f8c3fb61eaf1e0f50
김윰 <kimyumin71@gmail.com>
2026-03-11 13:27:31 +0900

    ExploreButton 스타일 수정 및 메인 페이지 레이아웃 구성

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,13 +1,41 @@
-//서비스의 메인 랜딩 페이지 컴포넌트
 import React from "react";
+import styled from "styled-components";
+import MainPoint1Section from "../components/MainPoint1Section"; 
+import MainPoint2Section from "../components/MainPoint2Section"; 
 import ExploreButton from "../components/ExploreButton";
 
 const MainPage = () => {
+  const handleExploreClick = () => {
+    console.log("구경하기 페이지로 이동");
+  };
+
   return (
-    <div>
-      <ExploreButton to="/list">구경해보기</ExploreButton>
-    </div>
+    <StyledPageContainer>
+      <MainPoint1Section />
+
+      <MainPoint2Section isReverse={true} />
+
+      <StyledButtonWrapper>
+        <ExploreButton to="/list" onClick={handleExploreClick}>
+          구경해보기
+        </ExploreButton>
+      </StyledButtonWrapper>
+    </StyledPageContainer>
   );
 };
+
+const StyledPageContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  gap: 30px; 
+  padding: 60px 0;
+  align-items: center;
+`;
+
+const StyledButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 48px;
+`;
 
 export default MainPage;
