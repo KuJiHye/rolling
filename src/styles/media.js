9a43cb9efd77dfcc88af12058d46d89eaed686f2
chaemin <sulv0309@gmail.com>
2026-03-12 15:33:14 +0900

    docs: 미디어 쿼리 기준 사이즈 수정 - labtop 기준 삭제 - size 수정

--- src/styles/media.js
+++ src/styles/media.js
@@ -1,11 +1,9 @@
 const size = {
-  mobile: '480px',
-  tablet: '768px',
-  labtop: '1024px'
+  mobile: '768px',
+  tablet: '1200px',
 }
 
 export const device = {
   mobile: `media screen and (max-width: ${size.mobile}`,
   tablet: `media screen and (max-width: ${size.tablet}`,
-  labtop: `media screen and (max-width: ${size.labtop}`,
 }
